aef0b94a9a39c56f049fa839870ebd6109baf49f
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-02-22 14:48:56 +0300

    banner in progress

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import classes from "./Banner.module.css"
+import { RiPlayFill } from "react-icons/ri";
 
 function Banner({banner, header}) {
   return (
     <div className={classes.container} style={{backgroundImage: `url(${banner})`}}>
-        <div className={classes.content}>
+        <div className={classes.overlay}>
+          <div className={classes.content}>
+            <div className={classes.year}>
+              <p>Working since 1992 </p>
+              <p className={classes.line}></p>
+            </div>
             <h3>{header}</h3>
+            <div className={classes.video}>
+                <p><RiPlayFill /></p>
+                <h5>WATCH INTRO VIDEO ABOUT US</h5>
+            </div>
+          </div>
+            
         </div>
         
     </div>
