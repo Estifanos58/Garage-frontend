bf70df99336eac6c7b52c736c6c3c061f1c549b8
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-02-23 10:38:32 +0300

    information component modile responsive

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -10,90 +10,92 @@ import cars from "../../assets/polished_car.jpg"
 function Information() {
   return (
     <div className={classes.Information}>
-        <div className={classes.left}>
-            <div className={classes.header}>
-                <h2>Why Choose Us</h2>
-                <div className={classes.line}></div>
+        <div className={classes.center}>
+            <div className={classes.left}>
+                <div className={classes.header}>
+                    <h2>Why Choose Us</h2>
+                    <div className={classes.line}></div>
+                </div>
+                <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward,a new normal that has evolved from generation heading towards.</p>
+                <ul>
+                    <li>
+                        <img src={mechanic} alt="Mechanic" />
+                        <p>Certified Expert Mechanics</p>
+                    </li>
+                    <li>
+                        <img src={tools} alt="Tools" />
+                        <p>Fast and Quality Service</p>
+                    </li>
+                    <li>
+                        <img src={tag} alt="Price Tag" />
+                        <p>Best Prices in Town</p>
+                    </li>
+                    <li>
+                        <img src={award} alt="Awards" />
+                        <p>Awarded Workshop</p>
+                    </li>
+                </ul>
             </div>
-            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward,a new normal that has evolved from generation heading towards.</p>
-            <ul>
-                <li>
-                    <img src={mechanic} alt="Mechanic" />
-                    <p>Certified Expert Mechanics</p>
-                </li>
-                <li>
-                    <img src={tools} alt="Tools" />
-                    <p>Fast and Quality Service</p>
-                </li>
-                <li>
-                    <img src={tag} alt="Price Tag" />
-                    <p>Best Prices in Town</p>
-                </li>
-                <li>
-                    <img src={award} alt="Awards" />
-                    <p>Awarded Workshop</p>
-                </li>
-            </ul>
-        </div>
-        <div className={classes.right}>
-            <div className={classes.header}>
-                <h2>Additional Services</h2>
-                <div className={classes.line}></div>
-            </div>
-            <div className={classes.container}>
-                <img src={cars} alt="" />
-                <div className={classes.descripition}>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>General Auto Repair & Maintenance</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Transmission Repair & Replacement</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Tire Repair and Replacement</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>State Emissions Inspection</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Break Job / Break Services</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Electrical Diagnostics</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Fuel System Repairs</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Starting and Changing Repair</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Streering and Suspension Work</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Emission Repair Facility</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Wheel Aligment</p>
-                    </div>
-                    <div className={classes.text}>
-                        <p className={classes.mark}><GiCheckMark /></p>
-                        <p>Computer Diagnostic Testing</p>
+            <div className={classes.right}>
+                <div className={classes.header}>
+                    <h2>Additional Services</h2>
+                    <div className={classes.line}></div>
+                </div>
+                <div className={classes.container}>
+                    <img src={cars} alt="" />
+                    <div className={classes.descripition}>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>General Auto Repair & Maintenance</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Transmission Repair & Replacement</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Tire Repair and Replacement</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>State Emissions Inspection</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Break Job / Break Services</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Electrical Diagnostics</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Fuel System Repairs</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Starting and Changing Repair</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Streering and Suspension Work</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Emission Repair Facility</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Wheel Aligment</p>
+                        </div>
+                        <div className={classes.text}>
+                            <p className={classes.mark}><GiCheckMark /></p>
+                            <p>Computer Diagnostic Testing</p>
+                        </div>
                     </div>
                 </div>
+            
             </div>
-           
         </div>
     </div>
   )
