6c5625d4297c619ad24858eadbc4b02421425115
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-02-24 12:05:30 +0300

    add_employee

--- src/components/Admin/AddEmployee/AddEmployee.jsx
+++ src/components/Admin/AddEmployee/AddEmployee.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import classes from "./AddEmployee.module.css"
 function AddEmployee() {
   return (
-    <div>AddEmployee</div>
+    <div className={classes.AddEmployee}>
+         <div className={classes.container}>
+            <div className={classes.header}> 
+                <h3>Add a new Employee</h3>
+                <div className={classes.line}></div>
+            </div>
+            <div className={classes.form}>
+               <div className={classes.form_login}>
+                <input type="text"  placeholder='Employee email'/>
+               </div>
+               <div className={classes.form_login}>
+                <input type="text" placeholder='Employee first name '/>
+               </div><div className={classes.form_login}>
+                <input type="text" placeholder='Employee last name'/>
+               </div><div className={classes.form_login}>
+                <input type="text" placeholder='Employee phone (555-555-5555'/>
+               </div>
+               <div className={`${classes.form_login} ${classes.select}`}>
+                    <select>
+                        <option value="employee">Employee</option>
+                        <option value="admin">Admin</option>
+                        <option value="manager">Manager</option>
+                    </select>
+               </div>
+               <button className={classes.btn}>ADD EMPLOYEE</button>
+            </div>
+         </div>
+    </div>
   )
 }
 
