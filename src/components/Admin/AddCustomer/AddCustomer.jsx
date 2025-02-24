8c2cd40d1f0ec692788506ff67e6bc26f52555ae
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-02-24 13:51:14 +0300

    add customer ui

--- src/components/Admin/AddCustomer/AddCustomer.jsx
+++ src/components/Admin/AddCustomer/AddCustomer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import classes from "./AddCustomer.module.css"
 function AddCustomer() {
   return (
-    <div>AddCustomer</div>
+    <div className={classes.AddCustomer}>
+        <div className={classes.container}>
+            <div className={classes.header}> 
+                <h3>Add a new customer</h3>
+                <div className={classes.line}></div>
+            </div>
+            <div className={classes.form}>
+               <div className={classes.form_login}>
+                <input type="text"  placeholder='Customer email'/>
+               </div>
+               <div className={classes.form_login}>
+                <input type="text" placeholder='Customer first name '/>
+               </div><div className={classes.form_login}>
+                <input type="text" placeholder='Customer last name'/>
+               </div><div className={classes.form_login}>
+                <input type="text" placeholder='Customer phone (555-555-5555'/>
+               </div>
+              
+               <button className={classes.btn}>ADD CUSTOMER</button>
+            </div>
+         </div>
+    </div>
   )
 }
 
