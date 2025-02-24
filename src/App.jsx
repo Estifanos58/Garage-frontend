98dfbb9b1b8215480cae0bd5c2e60475cd6e9287
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-02-24 11:25:15 +0300

    admin page configured

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Service from './pages/OurService/OurService'
 import About from './pages/About/About'
 import Contact from './pages/Contact/Contact'
 import Admin from './pages/Admin/Admin'
+import AddEmployee from './components/Admin/AddEmployee/AddEmployee'
+import AddCustomer from './components/Admin/AddCustomer/AddCustomer'
 
 function App() {
   return (
@@ -19,7 +21,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />}/>
         <Route path='/admin' element={<Admin />}>
-            
+            <Route path='add_employee' element={<AddEmployee />}/>
+            <Route path='add_customer' element={<AddCustomer />}/>
+            <Route path='*' element={<h1>Under constraction</h1>} />
         </Route>
       </Routes>
       <Footer />
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import classes from "./Admin.module.css"
+import { Outlet, Link } from 'react-router-dom'
 function Admin() {
   return (
     <div className={classes.admin}>
         <div className={classes.left}>
-
+            <h2>ADMIN MENU</h2>
+            <ul>
+                <Link ><li>Dashboard</li></Link>
+                <Link to={"order"}><li>Orders</li></Link>
+                <Link to={"new_order"}><li>New order</li></Link>
+                <Link to={"add_employee"}><li>Add employee</li></Link>
+                <Link to={"employee"}><li>Employees</li></Link>
+                <Link to={"add_customer"}><li>Add customer</li></Link>
+                <Link to={"customer"}><li>Customer</li></Link>
+                <Link to={"service"}><li>Service</li></Link>   
+                
+            </ul>
+            {/* <Link to={"add_employee"}><p>Add Employee</p></Link>
+            <Link to={"add_customer"}>Add Customer</Link> */}
         </div>
         <div className={classes.right}>
-            
+            <Outlet />
         </div>
     </div>
   )
